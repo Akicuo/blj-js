95a4bf20af02e14bb0b7300c0b172238b8c4ab10
Akicuo <user.of.replit@gmail.com>
2024-11-13 14:10:00 +0100

    awd

--- dom-parser/parser.js
+++ dom-parser/parser.js
@@ -1,18 +1,24 @@
-var result = "";
-
 function parseNode(startNode) {
-    var result = ""
-    for (var i = 0; i < startNode.childNodes.length; i++) {
-        var child = startNode.childNodes[i];}
-        result += "<tr>"
-        + "<td>" + child.nodeType + "</td>"
-        + "<td>" + child.nodeName + "</td>"
-        + "<td>" + child.nodeValue + "</td>"
-        + "<td>" + child.text + "</td>"
-        + "</tr>";
-    //todo
-    //für jeden Kindknoten ab "startNode" eine Tabellenzeile (<tr><td>....</tr>) 
-    //mit den gemäss Aufgabenstellung gewünschten Infos erstellen
+    var result = "";
+    var elements = startNode.childNodes;
+    
+   
+    for (var child of elements) {
+        travelThroughNode(child);
+    }
 
     return "<table><tr><th>nodeType</th><th>nodeName</th><th>nodeValue</th><th>textContent</th></tr>" + result + "</table>";
+    function travelThroughNode(node) {
+        result += "<tr>"
+        + "<td>" + node.nodeType + "</td>"
+        + "<td>" + node.nodeName + "</td>"
+        + "<td>" + node.nodeValue + "</td>"
+        + "<td>" + node.textContent + "</td>"
+        + "</tr>";
+        if (node.childNodes) {
+            for (var child of node.childNodes) {
+                travelThroughNode(child);
+            }
+        }
+    }
 }
